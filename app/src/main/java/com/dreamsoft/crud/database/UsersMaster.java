b6f64f3fd4cc94722d1e2a51df20e820f287adf8
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-20 08:19:01 +0530

    Created and Modified UsersMaster.java file

---
+++ app/src/main/java/com/dreamsoft/crud/database/UsersMaster.java
@@ -0,0 +1,13 @@
+package com.sliit.crud.database;
+
+import android.provider.BaseColumns;
+
+public final class UsersMaster {
+    private UsersMaster(){}
+
+    public static class Users implements BaseColumns{
+        public static final String TABLE_NAME = "users";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_PASSWORD = "password";
+    }
+}
